361d71fec4ab69fb27524498c8df3c56303e42a4
Ritesh kumar sah <riteshkumar555sah@gmail.com>
2024-06-06 18:51:37 +0530

    Add url...

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const Review = require("./Models/ReviewModel")
 
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://riteshdev-bookingapp.netlify.app"],
+  origin: ["http://localhost:5173", "https://riteshdev-bookingapp.netlify.app", "https://riteshdevbookingapp01.netlify.app/"],
   methods: "GET,POST,PATCH,DELETE,UPDATE,PUT,HEAD ",
   credentials: true,
 };
